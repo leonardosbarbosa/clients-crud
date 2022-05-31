b53cda70755349acfcb84a7045f17ec7fb3b65a2
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2022-05-30 21:30:47 -0300

    Find client by id and exception handler

--- src/main/java/com/leonardosbarbosa/clientscrud/resources/ClientResource.java
+++ src/main/java/com/leonardosbarbosa/clientscrud/resources/ClientResource.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -29,5 +26,11 @@ public class ClientResource {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         Page<ClientDTO> clientsPage = service.findAllClientsPaged(pageRequest);
         return ResponseEntity.ok().body(clientsPage);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findClientById(@PathVariable Long id) {
+        ClientDTO client = service.findClientById(id);
+        return ResponseEntity.ok().body(client);
     }
 }
--- src/main/java/com/leonardosbarbosa/clientscrud/services/ClientService.java
+++ src/main/java/com/leonardosbarbosa/clientscrud/services/ClientService.java
@@ -3,11 +3,14 @@ package com.leonardosbarbosa.clientscrud.services;
 import com.leonardosbarbosa.clientscrud.dto.ClientDTO;
 import com.leonardosbarbosa.clientscrud.entities.Client;
 import com.leonardosbarbosa.clientscrud.repositories.ClientRepository;
+import com.leonardosbarbosa.clientscrud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -19,5 +22,11 @@ public class ClientService {
     public Page<ClientDTO> findAllClientsPaged(PageRequest pageRequest) {
         Page<Client> clientsList = repository.findAll(pageRequest);
         return clientsList.map(entity -> new ClientDTO(entity));
+    }
+
+    public ClientDTO findClientById(Long id) {
+        Optional<Client> client = repository.findById(id);
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException(String.format("Client with id %d not found", id)));
+        return new ClientDTO(entity);
     }
 }
